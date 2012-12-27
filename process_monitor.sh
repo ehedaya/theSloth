0a6e38767f9f2d01e4f0c7db5b1196baccbcaf3a
Emil Hedaya <ehedaya@gmail.com>
2012-12-26 20:38:16 -0800

    Auto-reboot if process is up but no recent activity

--- process_monitor.sh
+++ process_monitor.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env sh
-cd /home/thephishvps/bots/
 m=`pgrep -f "theSloth/bot.js" | wc -l`
 if  [ $m -gt 0 ]
 then
 	m=`pgrep -f "theSloth/bot.js"`
 	echo "Process $m found"
+	now=`date +"%s"`
+	last=`cat /home/thephishvps/tmp/theSloth`
+	diff=`expr $now - $last`;
+	if [ $diff -gt 360 ] 
+	then
+		kill $m
+		/usr/bin/nohup /home/thephishvps/usr/local/bin/node /home/thephishvps/bots/theSloth/bot.js >> /home/thephishvps/bots/theSloth.out 2>&1&
+	else 
+		echo "Last activity $diff second(s) ago"
+	fi
+		
 else
 	/usr/bin/nohup /home/thephishvps/usr/local/bin/node /home/thephishvps/bots/theSloth/bot.js >> /home/thephishvps/bots/theSloth.out 2>&1&
 fi
+
