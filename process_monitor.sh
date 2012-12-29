275fa2fc745929a702e7023833992e4c9ea1bff8
Emil Hedaya <emil@redmedia.com>
2012-12-28 20:19:56 -0500

    Send an email if process reboots

--- process_monitor.sh
+++ process_monitor.sh
@@ -9,13 +9,15 @@ then
 	diff=`expr $now - $last`;
 	if [ $diff -gt 360 ] 
 	then
+        echo "Last activity $diff second(s) ago, rebooting"
 		kill $m
 		/usr/bin/nohup /home/thephishvps/usr/local/bin/node /home/thephishvps/bots/theSloth/bot.js >> /home/thephishvps/bots/theSloth.out 2>&1&
+		mail -s "theSloth was restarted (last activity $diff seconds ago)" emil@thephish.fm
 	else 
 		echo "Last activity $diff second(s) ago"
 	fi
-		
 else
 	/usr/bin/nohup /home/thephishvps/usr/local/bin/node /home/thephishvps/bots/theSloth/bot.js >> /home/thephishvps/bots/theSloth.out 2>&1&
+	mail -s "theSloth process died, restarted" emil@thephish.fm
 fi
 
