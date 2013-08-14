99aba312ed096615f04db4d332cd2ff8e241fb64
Emil Hedaya <ehedaya@gmail.com>
2013-08-14 23:19:15 +0000

    TDG closes #36

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ function isJsonString(str) {
     return true;
 }
 
-function authKey() {
+function authKey() { 
 	return md5(getEpoch()+apikey);
 }
 
@@ -337,7 +337,7 @@ bot.on('speak', function (data) {
 		{ trigger: new RegExp('^!new$', 'i'), response: 'http://bit.ly/slothNew'},
 		{ trigger: new RegExp('^!pnet$', 'i'), response: 'To link your stats, Fan me and then send a PM with !pnet:username (replace username with your .net username). If you update your stats on Phish.net, PM me !pnet to refresh.'},
 		{ trigger: new RegExp('^!attendance$', 'i'), response: 'http://thephish.fm/attendance'},
-                { trigger: new RegExp('^!replayroom$', 'i'), response: 'http://preview.tinyurl.com/thephishreplayroom'},
+        { trigger: new RegExp('^!replayroom$', 'i'), response: 'http://preview.tinyurl.com/thephishreplayroom'},
 		{ trigger: new RegExp('^!tickets$', 'i'), response: 'http://thephish.fm/tickets'}
 	];
 	for(t=0;t<chatResponses.length;t++) {
@@ -566,6 +566,22 @@ bot.on('speak', function (data) {
    				bot.speak('http://phishtracks.com/shows/'+showdate);
    			} else {
 				bot.speak('I couldn\'t find a date in any fields: '+data.room.metadata.current_song.metadata.artist+' '+data.room.metadata.current_song.metadata.song+' '+data.room.metadata.current_song.metadata.album);
+			}
+   		});
+   }
+   if (text.match(/^\!tdg$/)) {	 
+   		bot.roomInfo(true, function(data) {
+   			var song = data.room.metadata.current_song.metadata.song;
+   			var artist = data.room.metadata.current_song.metadata.artist;
+   			var album = data.room.metadata.current_song.metadata.album;
+   			if (showdate = parseDate(song+' '+artist+' '+album)) {
+				if( song.substr('ghost') ) {
+	   				bot.speak('You might be able to read about this :ghost: here: http://lawnmemo.com/'+showdate);
+	   			} else {
+	   				bot.speak('Definitely not a :ghost: .. or maybe I\'m losing it'); 
+	   			}
+   			} else {
+				bot.speak('Not a :ghost:');
 			}
    		});
    }
